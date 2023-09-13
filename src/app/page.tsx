20d575c5032f5c8b5012ebdc99e7d8b72a07366a
Alex Marinov <alex@diylab.uk>
2023-09-13 23:47:31 +0100

    passing props between components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
+"use client";
+
 import CodeEditor from "@/components/CodeEditor";
 import ThreeCanvas from "@/components/ThreeCanvas";
+import { useState } from "react";
 
 export default function Home() {
+  const [consoleText, setConsoleText] = useState<string>("Console");
+
+  const updateConsoleText = (e: string) => {
+    setConsoleText(`${consoleText}\n${e}`);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <div className="p-2">CadQuery Studio</div>
       <div className="flex-1 flex flex-row">
         <div className="w-1/2 bg-gray-800">
-          <CodeEditor />
+          <CodeEditor updateConsoleText={updateConsoleText} />
         </div>
         <div className="w-1/2 p-2 bg-gray-600">
           <ThreeCanvas />
         </div>
       </div>
-      <div className="p-2 h-24 bg-gray-900">Console</div>
+      <div className="p-2 h-24 bg-gray-900">
+        <pre>{consoleText}</pre>
+      </div>
     </div>
   );
 }
--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,14 +1,37 @@
 "use client";
 
 import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 import { MdPlayCircleOutline, MdClear } from "react-icons/md";
 
-export default function CodeEditor() {
+export default function CodeEditor({
+  updateConsoleText,
+}: {
+  updateConsoleText: (updateConsoleText: string) => void;
+}) {
+  // const [consoleText, setConsoleText] = useState<string>("Console");
+
+  const initialCode =
+    'import cadquery as cq\n\nresult = cq.Workplane("XY").box(2, 2, 2)';
+
+  const [code, setCode] = useState(initialCode);
+
+  const runCode = () => {
+    console.log("inside the runCode");
+    updateConsoleText("inside the runCode");
+  };
+
+  const clearCode = () => {
+    console.log("inside the clearCode");
+    updateConsoleText("inside the clearCode");
+    setCode("");
+  };
   return (
     <div className="h-full flex flex-col">
       <div className="flex-1">
         <Editor
           defaultLanguage="python"
+          value={code}
           theme="vs-dark"
           options={{
             minimap: {
@@ -18,10 +41,22 @@ export default function CodeEditor() {
         />
       </div>
       <div className="flex flex-row bg-gray-800 p-2 w-full">
-        <button className="text-2xl space-x-3 mr-2 font-semibold rounded-sm ring-1 ring-gray-600 px-6 py-2 hover:bg-gray-600  hover:text-white hover:ring-slate-300">
+        <button
+          className="text-2xl space-x-3 mr-2 font-semibold rounded-sm ring-1 ring-gray-600 px-6 py-2 hover:bg-gray-600  hover:text-white hover:ring-slate-300"
+          type="button"
+          onClick={() => {
+            runCode();
+          }}
+        >
           <MdPlayCircleOutline />
         </button>
-        <button className="text-2xl space-x-3 mr-2 font-semibold rounded-sm ring-1 ring-gray-600 px-6 py-2 hover:bg-gray-600  hover:text-white hover:ring-slate-300">
+        <button
+          className="text-2xl space-x-3 mr-2 font-semibold rounded-sm ring-1 ring-gray-600 px-6 py-2 hover:bg-gray-600  hover:text-white hover:ring-slate-300"
+          type="button"
+          onClick={() => {
+            clearCode();
+          }}
+        >
           <MdClear />
         </button>
       </div>
